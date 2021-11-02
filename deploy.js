9784d01692d9b067413dc8b1f86eeda282d02308
Carson Minihan <cdminihan01@gmail.com>
2021-11-02 11:32:32 -0700

    updated ip

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.223.21.100',
+      host: '18.220.119.126',
       username: 'ubuntu',
       privateKey: 'deployment-final-key.pem'
     })
